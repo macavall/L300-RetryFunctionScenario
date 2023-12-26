daf43b10296841b82584162b6c81900c0298137a
Matthew Cavallo <macavall@microsoft.com>
2023-12-25 18:19:22 -0600

    first

---
+++ http1.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace TestIso7FA
+{
+    public class http1
+    {
+        private readonly ILogger _logger;
+        private static readonly ManualResetEventSlim eventSlim = new ManualResetEventSlim(false);
+        private static int threadCount = 600;
+
+        public http1(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<http1>();
+        }
+
+        [Function("http1")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            StartHighThreadCount();
+
+            response.WriteString(Process.GetCurrentProcess().Id.ToString());
+
+            return response;
+        }
+
+        public void StartHighThreadCount()
+        {
+            for (int i = 0; i < threadCount; i++)
+            {
+                Thread thread = new Thread(DoWork);
+                thread.Start(i);
+            }
+
+            // Signal the event to start all the threads.
+            eventSlim.Set();
+
+            // Keep the main thread alive until all other threads are done.
+            while (threadCount > 0)
+            {
+                Thread.Sleep(100);
+            }
+
+            eventSlim.Dispose();
+        }
+
+        public void DoWork(object data)
+        {
+            int threadNumber = (int)data;
+
+            // Wait for the event to be signaled.
+            eventSlim.Wait();
+
+            Console.WriteLine($"Thread {threadNumber} is doing some work.");
+
+            // Simulate some work.
+            Thread.Sleep(600000);
+
+            Interlocked.Decrement(ref threadCount);
+        }
+    }
+}
---
+++ http2.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace TestIso7FA
+{
+    public class http2
+    {
+        private readonly ILogger _logger;
+
+        public http2(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<http2>();
+        }
+
+        [Function("http2")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString(Process.GetCurrentProcess().Id.ToString());
+
+            return response;
+        }
+    }
+}
